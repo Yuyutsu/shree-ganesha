756958d3a3faf395b8501f656562e05e2fe16877
Amol Chavan <amolchavan0619@gmail.com>
2014-07-03 23:58:15 +0530

    done first assigment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,12 @@
 class ArticlesController < ApplicationController
+http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
+  def index
+    @articles = Article.all
+  end
+ 
+  # snipped for brevity
+
 def new
   @article = Article.new
 end
@@ -17,9 +25,7 @@ def show
   @article = Article.find(params[:id])
 end	
 	
-def index
-  @articles = Article.all
-end
+
 def edit
   @article = Article.find(params[:id])
 end
